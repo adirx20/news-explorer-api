a050b13c6216efa066db5d9b489711fb548efe99
adirx20 <87542990+adirx20@users.noreply.github.com>
2022-09-09 17:53:21 +0300

    add auth and articles controllers and routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const { celebrate, Joi } = require('celebrate');
+const { validateURL } = require('../middlewares/validateURL');
+const {
+  createUser,
+  login,
+} = require('../controllers/auth');
+
+const router = express.Router();
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+module.exports = router;
